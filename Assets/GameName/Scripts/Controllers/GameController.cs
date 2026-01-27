98b45847cc730d121ad975a9a529fdc684d498e5
Cậu vàng <huuminhgm@gmail.com>
2026-01-28 00:56:01 +0700

    update audio sys

--- Assets/GameName/Scripts/Controllers/GameController.cs
+++ Assets/GameName/Scripts/Controllers/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
     [SerializeField] private List<GameObject> _levelPrefabs;
     [SerializeField] private GameObject _playerPrefab;
     [SerializeField] private List<GameObject> _skillPrefab; // Get skill by index
+    [SerializeField] AudioDataSO _audioDataSO;
     private bool _isLoadingComplete = false;
     private bool _isPaused = false;
     private bool _isGameOver = false;
@@ -15,7 +16,14 @@ public class GameController : MonoBehaviour
     private DataManager _dataManager = new DataManager();
     async void Start()
     {
-        _dataManager.LoadPlayerData();        
+        AudioManager.Instance.SetAudioData(_audioDataSO);
+        for (int i = 0; i < 100; i++)
+        {
+            await Task.Delay(1000); 
+            PlayRandomSound();
+        }
+
+        _dataManager.LoadPlayerData();
         await LoadLevel(_dataManager.PlayerProgressionData.CurrentLevel);
         _isLoadingComplete = true;
     }
@@ -24,10 +32,10 @@ public class GameController : MonoBehaviour
     {
         // TO DO: Load level data from DataManager and initialize level
     }
-    
+
     public void ResetLevel()
     {
-        
+
     }
 
     void Update()
@@ -36,5 +44,25 @@ public class GameController : MonoBehaviour
         if (_isPaused || _isGameOver) return;
 
         //TO DO: Call every child entity update
+    }
+
+    public void PlayRandomSound()
+    {
+        int ran = Random.Range(0, 4);
+        switch (ran)
+        {
+            case 0:
+                AudioManager.Instance.PlaySound(TemplateAudioConstants.eating);
+                break;
+            case 1:
+                AudioManager.Instance.PlaySound(TemplateAudioConstants.gaining_experience);
+                break;
+            case 2:
+                AudioManager.Instance.PlaySound(TemplateAudioConstants.open_chest);
+                break;
+            case 3:
+                AudioManager.Instance.PlaySound(TemplateAudioConstants.zombie_roar);
+                break;
+        }
     }
 }
