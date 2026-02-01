9a1e0a50752b1c86d94eae8f66bade64b7e87c18
Truong <schoolpear14@gmail.com>
2026-02-01 07:44:18 +0700

    update

--- Assets/GameName/DirtyPlace/TryAgainController.cs
+++ Assets/GameName/DirtyPlace/TryAgainController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class TryAgainController : MonoBehaviour
 {
     public Button tryAgainButton;
+    public Button mainMenuButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +13,11 @@ public class TryAgainController : MonoBehaviour
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("MiniGameScene");
             Debug.Log("Thử lại trò chơi!");
+        });
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MenuScene");
+            Debug.Log("Quay về menu chính!");
         });
     }
 
